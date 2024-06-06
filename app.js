65e1857d513368275409ab41dde5857a0ab56241
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-06-06 16:04:43 -0400

    Updated middleware functions for security

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = process.env.MONGODB_URI || "your_mongodb_connection_string";
+const mongoDB = process.env.MONGODB_URI || "mongodb+srv://your_user_name:your_password@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority";
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB, {
